fa8741a1b38a703b541c0d2004b3173cb0dc6909
hkctkuy <hkctkuy@gmail.com>
2022-07-14 18:52:17 +0300

    fix find_text_fuzzer.cc

--- glib/tests/fuzzing/find_text_fuzzer.cc
+++ glib/tests/fuzzing/find_text_fuzzer.cc
@@ -10,6 +10,10 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
     PopplerPage *page;
     char *buf;
     int npages;
+    
+    if(!g_utf8_validate((char *)data, size, NULL)) {
+        return 0;
+    }
 
     doc = poppler_document_new_from_data((char *)data, size, NULL, &err);
     if (doc == NULL) {
